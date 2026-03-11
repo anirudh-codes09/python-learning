ec551f907b30c806bc0ae393abd6a498adf9fdb6
Anirudh Adusumilli <ani.adusumilli09@gmail.com>
2026-03-11 20:56:08 +0530

    My first python code

--- Functions.py
+++ Functions.py
@@ -3,4 +3,5 @@ city = input("Enter your city: ")
 gender = input("Enter your gender: ")
 sport = input("Enter the sport you play: ")
 
+
 print(name,'lives in',city, 'he/she is a' ,gender, 'and he/she playes',sport)
