a3d09287c9fe421b87a4dcc157566c01cb9207e1
Anirudh Adusumilli <ani.adusumilli09@gmail.com>
2026-03-19 20:32:34 +0530

    Today, I learned about a custom function. A custom function is used can be used to do multiple tasks.

---
+++ CustomFucntions.py
@@ -0,0 +1,8 @@
+def inside():
+    global y
+    y = 30
+    print("Value of y inside:", y)
+
+
+inside()
+print("Value of y outside:", y)
---
+++ CustomFunction2.py
@@ -0,0 +1,6 @@
+def square_cube(x):
+    return x**2, x**3
+
+square,cube = square_cube(9)
+print(square)
+print(cube)
